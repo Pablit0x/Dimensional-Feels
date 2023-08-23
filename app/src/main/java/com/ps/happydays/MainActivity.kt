f53bb084799c6c8ebaeb9cd9ef1bceaa326e11d7
noner <pawel495@interia.pl>
2023-08-23 22:14:38 +0100

    Implementation of Splash Screen

--- app/src/main/java/com/ps/happydays/MainActivity.kt
+++ app/src/main/java/com/ps/happydays/MainActivity.kt
@@ -10,19 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ps.happydays.ui.theme.HappyDaysTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             HappyDaysTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
+
             }
         }
     }

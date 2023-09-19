57ea4442f9ae8fbeca1722dbf32a5e273dfdaba3
noner <pawel495@interia.pl>
2023-09-19 13:31:14 +0100

    Gradle updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,29 +59,29 @@ dependencies {
     implementation("com.airbnb.android:lottie-compose:6.0.0")
 
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.foundation:foundation:1.5.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.foundation:foundation:1.5.2")
+    implementation(platform("androidx.compose:compose-bom:2023.09.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 
     // Compose Navigation
-    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
 
     // Firebase
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
 
     // Room components
@@ -90,13 +90,13 @@ dependencies {
     implementation("androidx.room:room-ktx:2.5.2")
 
     // Runtime Compose
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
     // Splash API
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Mongo DB Realm
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
     implementation("io.realm.kotlin:library-sync:1.10.0")
 
     // Dagger Hilt
@@ -114,6 +114,11 @@ dependencies {
     implementation("com.github.stevdza-san:OneTapCompose:1.0.3")
 
     // Extended Icons
-    implementation("androidx.compose.material:material-icons-extended:1.5.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.2")
+
+    // Google Fonts
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.2")
+
+
 
 }

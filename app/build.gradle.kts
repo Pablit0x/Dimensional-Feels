800454dafc786492c3057d2d6979cbc920113c96
noner <pawel495@interia.pl>
2023-10-30 16:11:30 +0000

    Updating gradle versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.foundation:foundation:1.5.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.foundation:foundation:1.5.4")
     implementation(platform("androidx.compose:compose-bom:2023.09.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -78,16 +78,16 @@ dependencies {
 
 
     // Compose Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
     // Firebase
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
-    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
+    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
 
     // Room components
-    implementation("androidx.room:room-runtime:2.5.2")
-    ksp("androidx.room:room-compiler:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2")
+    implementation("androidx.room:room-runtime:2.6.0")
+    ksp("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
 
     // Runtime Compose
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
@@ -111,13 +111,13 @@ dependencies {
     implementation("com.github.stevdza-san:MessageBarCompose:1.0.5")
 
     // One-Tap Compose
-    implementation("com.github.stevdza-san:OneTapCompose:1.0.3")
+    implementation("com.github.stevdza-san:OneTapCompose:1.0.8")
 
     // Extended Icons
-    implementation("androidx.compose.material:material-icons-extended:1.5.2")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
     // Google Fonts
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.2")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
 
     // Drawing Tool
     implementation("io.ak1:drawbox:1.0.3")

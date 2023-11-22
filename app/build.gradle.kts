3c2aabf31de91a1347a0f79e487cb67943ac48e8
noner <pawel495@interia.pl>
2023-11-22 23:05:51 +0100

    Updating gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.compose.foundation:foundation:1.5.4")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
@@ -79,7 +79,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
     // Firebase
-    implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
 
     // Room components
